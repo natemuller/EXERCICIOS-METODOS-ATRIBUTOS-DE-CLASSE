36fff963bc7e8d4efb534fe3af4b707b3227d7d4
natan gostosao <natan.muller@edu.pucrs.br>
2025-04-02 20:20:00 -0300

    primeira versao do exercicio2 feita, cheia de bugs

--- Exercicio2/src/Senha.java
+++ Exercicio2/src/Senha.java
@@ -1,5 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Senha {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    private static int senhaAtual; // atributo de classe
+    private static int minhaSenha; // atributo de instancia
+
+    public Senha() {
+        senhaAtual = new Random().nextInt(1000);
+        minhaSenha = senhaAtual;
+    } 
+
+    public static int getMinhaSenha() {
+        return minhaSenha;
     }
+
+    public static int getSenhaAtual() {
+        return senhaAtual;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Digite o tamanho do vetor de senhas:\n");
+        int tam = scanner.nextInt();
+
+        Senha[] senhas = new Senha[tam];
+        
+        for (int i = 0; i < tam; i++) {
+            senhas[i] = new Senha();
+        }
+        
+        for (Senha senha : senhas) {
+            System.out.printf("Senha: %d\n", senha.getMinhaSenha());
+        }
+        scanner.close();
+    }   
 }
